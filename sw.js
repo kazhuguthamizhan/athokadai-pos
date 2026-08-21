543b24cd001057f533914a744b69641cc93ce42d
kazhuguthamizhan <144526778+kazhuguthamizhan@users.noreply.github.com>
2026-08-21 11:39:44 +0530

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'athokadai-pos-v5';
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => caches.delete(key)));
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
